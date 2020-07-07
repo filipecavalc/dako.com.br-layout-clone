144d018ee0102172993ac19b91b90553328afc24
Filipe Cavalcante <filipecavalc@gmail.com>
2020-07-07 00:18:15 -0300

    carousel / sessão apelo comercial
    implementado sessão com informações de apelo comercial

--- js/carousel.js
+++ js/carousel.js
@@ -1,22 +1,31 @@
-new Glide('.glide', {
-    autoplay: 4000
+new Glide(".glide", {
+  autoplay: 4000,
 }).mount();
 
-new Glide('.glide-2', {
-    bound: true,
-    perView: 4,
-    breakpoints: {
-        767: {perView: 2},
-        1199: {perView: 3}
-    }
+new Glide(".glide-2", {
+  bound: true,
+  perView: 4,
+  breakpoints: {
+    767: { perView: 2 },
+    1199: { perView: 3 },
+  },
 }).mount();
 
-new Glide('.commercial-appeals', {
-    bound: true,
-    perView: 3,
-    dragThreshold: false,
-    breakpoints: {
-        519: {perView: 1},
-        1023: {perView: 2},
-    }
+new Glide(".commercial-appeals", {
+  bound: true,
+  perView: 3,
+  dragThreshold: false,
+  swipeThreshold: false,
+  breakpoints: {
+    519: {
+      dragThreshold: true,
+      swipeThreshold: true,
+      perView: 1,
+    },
+    1023: {
+      dragThreshold: true,
+      swipeThreshold: true,
+      perView: 2,
+    },
+  },
 }).mount();
